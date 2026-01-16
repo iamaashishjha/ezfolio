aa39e07232da82c4ba6dea354c84d01e3375cf28
iamaashishjha <mail.aashishjha@gmail.com>
2026-01-16 21:26:10 +0545

    updated logging stack

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => explode(',', env('LOG_STACK', 'single')),
+            'channels' => explode(',', env('LOG_STACK', 'daily')),
             'ignore_exceptions' => false,
         ],
 
